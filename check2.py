dfe2f355f7887a7a05c627bb715549338aa11fd2
Vox-Mortem <darkfall.ss@gmail.com>
2024-08-07 01:08:33 +0300

    Add files via upload

---
+++ check2.py
@@ -0,0 +1,6 @@
+number_of_tasks_completed = 12
+number_of_hours_spent = 1.5
+course_name = 'Python'
+time_per_task = number_of_hours_spent/number_of_tasks_completed
+
+print('Курс:', course_name, ', всего задач:', number_of_tasks_completed, ', затрачено часов:', number_of_hours_spent, ', среднее время выполнения ', time_per_task, 'часа.')
