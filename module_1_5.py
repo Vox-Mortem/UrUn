329858a5fd64f9f63147beefcf5e110686548ec3
Vox-Mortem <darkfall.ss@gmail.com>
2024-08-09 02:17:48 +0300

    Add files via upload

---
+++ module_1_5.py
@@ -0,0 +1,8 @@
+immutable_var = tuple = 32, 'Deliverance', True
+print(immutable_var)
+# tuple[0] = 11
+# print(immutable_var) #TypeError: 'tuple' object does not support item assignment
+mutable_list = tuple =([44, 'Corrupt', False ], 322)
+print(mutable_list)
+tuple[0][1] = 'Curse'
+print(mutable_list)
