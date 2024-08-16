f81e4d8a3fc9d3a4d78edcc77c355de07e90d873
Vox-Mortem <darkfall.ss@gmail.com>
2024-08-17 02:21:11 +0300

    Add files via upload

---
+++ module_2_hard.py
@@ -0,0 +1,12 @@
+import random
+
+n = random.randint(3, 20)
+result = []
+for i in range(1, n):
+    for j in range(i + 1, n):
+        if n % (i + j) == 0:
+            result.append(i)
+            result.append(j)
+
+print('Выпавшее число:', n)
+print('Решение:', *result, sep='')
